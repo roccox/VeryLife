b8cacfaa4e5c20b3f956b36bdbd91e20305bae21
Rock <rock@rockdemacbook.local>
2012-01-10 10:18:29 +0800

    Initial Commit

---
+++ MyVeryLife/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyVeryLife
+//
+//  Created by Rock on 12-1-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
