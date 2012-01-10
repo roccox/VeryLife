ece5d2cf5e03340a0a34b0e26bb4412bd06af1c2
Rock <rock@rockdemacbook.local>
2012-01-10 10:53:53 +0800

    Rock：Refactor class and Add Second Tier Controllers.

---
+++ MyVeryLife/DetailInfoWeb.h
@@ -0,0 +1,13 @@
+//
+//  DetailInfoWeb.h
+//  MyVeryLife
+//
+//  Created by Rock on 12-1-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailInfoWeb : UIViewController
+
+@end
---
+++ MyVeryLife/SearchTabController.h
@@ -0,0 +1,13 @@
+//
+//  SearchTabController.h
+//  MyVeryLife
+//
+//  Created by Rock on 12-1-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchTabController : UIViewController
+
+@end
